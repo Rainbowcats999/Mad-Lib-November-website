7b26ff390cafaaa62d86406ecb2b6733c999f7a9
Rainbowcats999 <526erizov@frhsd.com>
2024-11-12 09:24:38 -0500

    Added more prompts, a console, and a document

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let userName = prompt('WHAT IS YOUR NAME?')
 let userQuest = prompt ('What is your quest?')
 let userFavoriteColor = prompt('What is your favorite color?')
+let swallowedAirspeed = prompt ('What is the speed of an uneven swallowed?')
 let userAge = prompt ('What is your age?')
-console.log (userName)
+
+let myStory =
+`<p> Hello ${userName}! Answer me these questions 3 and the other side you'll see.</p>
+
+<p> I see you have already answered, so your quest is to ${userQuest} </p>`
+
+console.log (myStory)
+
+document.getElementById()
